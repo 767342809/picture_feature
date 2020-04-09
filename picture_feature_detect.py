b5a80572fd51052b76d2374098adaa3a2f92542f
liangyue <liangyue@yit.com>
2020-04-09 15:06:56 +0800

    hog feature detect

--- picture_feature_detect.py
+++ picture_feature_detect.py
@@ -1,4 +1,8 @@
 import ssl
+import csv
+import pickle
+import logging
+from enum import Enum
 import matplotlib.pyplot as plt
 
 from skimage.feature import hog
@@ -9,6 +13,14 @@ import numpy as np
 from skimage.io import imread
 
 ssl._create_default_https_context = ssl._create_unverified_context
+
+ROOT = "/Users/liangyue/Documents/frozen_model_vgg_16/"
+ORIGINAL_DATA_PATH = ROOT + "url_file/"
+FEATURE_PATH = ORIGINAL_DATA_PATH + "feature_results/"
+RESULT_PATH = ROOT + "vector_package/"
+
+logging.basicConfig(level=logging.INFO, format='%(name)s - %(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def look_hog_picture(img_path, is_resize):
@@ -38,24 +50,54 @@ def detect_hog_feature(img_path, is_resize: bool=True):
     return fd, hog_image, image
 
 
-if __name__ == "__main__":
+class ExtractFeatureMethod(Enum):
+    HOG = 1
+
+
+def write_vector_to_csv(method: Enum):
+    with open(ORIGINAL_DATA_PATH + "post_url_dict.p", "rb") as p:
+        cate_img_url_dict = pickle.load(p)
+
+    count = 0
+    feature_dict = {}
+    for post_id, url in cate_img_url_dict.items():
+        try:
+            feature, _, _ = detect_hog_feature(url)
+            feature_dict[post_id] = feature
+            count += 1
+        except Exception as e:
+            logger.info(post_id, url, e)
+        if count % 10 == 0:
+            logger.info(f"detect {count} post features. ")
+
+    with open(FEATURE_PATH+method.name+"feature.p", "wb") as f:
+        pickle.dump(feature_dict, f)
+
+
+def test():
     img_file = "https://wx1.sinaimg.cn/orj1080/53db7999gy1gd0cxmhljwj215o0rs4qp.jpg"
     img_file2 = "./tfrecord/resize.jpg"
     img_file3 = "./test_data/puzzle.jpeg"
     fd1, _, _ = detect_hog_feature(img_file)
     fd2, _, _ = detect_hog_feature(img_file2)
     fd3, _, _ = detect_hog_feature(img_file3)
-    all = np.array([fd1.tolist(), fd2.tolist(), fd3])
-    print(all)
+    all = np.array(np.matrix([fd1.tolist(), fd2.tolist(), fd3]))
+    print(all.shape)
     print(np.corrcoef(all))
 
-
     p = all
-    q = fd2
-    BC = np.sum(np.sqrt(p * q), axis=1)
+    q = np.matrix(fd2)
+    print("q: ", q)
+    BC = np.sum(np.sqrt(np.multiply(p, q)), axis=1)
     print("BC: ", BC)
-    men = np.mean(p, axis=1) * np.mean(q) * np.power(q.shape[0], 2)
+    men = np.matrix(np.mean(p, axis=1) * np.mean(q) * np.power(q.shape[1], 2)).reshape(p.shape[0], 1)
     print("men: ", men)
     s = 1 - (BC / np.sqrt(men))
-    d = np.sqrt(s)
+    d = np.sqrt(s).reshape(1, p.shape[0]).tolist()[0]
     print(d)
+    sor = np.argsort(d)
+    print(sor)
+
+
+if __name__ == "__main__":
+    write_vector_to_csv(ExtractFeatureMethod.HOG)
