e5ba5a63595a4fc857018a3334074261bc84196b
liangyue <liangyue@yit.com>
2020-04-20 19:31:14 +0800

    test

--- prepare_tfrecord_files.py
+++ prepare_tfrecord_files.py
@@ -1,6 +1,7 @@
 import os
 import ssl
 import oss2
+import pickle
 import urllib.request
 import tensorflow as tf
 
@@ -28,9 +29,13 @@ def image_to_tfrecord(img_file, label, image_id, local_tf, is_save_show_picture=
         img_raw = encoded_image.eval()
 
         if is_save_show_picture:
-            local_picture_file = os.path.join(LOCAL_TFRECORD_PATH, image_id, 'resize.jpg')
-            with tf.gfile.GFile(local_picture_file, 'wb') as f:
-                f.write(img_raw)
+            folder = "./resize"
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            local_picture_file = os.path.join(folder, image_id+'+resize.jpg')
+            if not os.path.exists(local_picture_file):
+                with tf.gfile.GFile(local_picture_file, 'wb') as f:
+                    f.write(img_raw)
 
         writer = tf.python_io.TFRecordWriter(local_tf)
         example = tf.train.Example(features=tf.train.Features(feature={
@@ -52,16 +57,31 @@ def connect_pai_oss():
 
 def prepare_train_data_in_oss():
     bucket = connect_pai_oss()
-    raw = [
-        (12332, "https://wx1.sinaimg.cn/orj1080/53db7999gy1gd0cxmhljwj215o0rs4qp.jpg", 0),
-        (12432, "/Users/liangyue/Documents/frozen_model_vgg_16/2.jpg", 1)
-    ]
-    for img_id, img_url, label in raw:
+    train_df_fn = "/Users/liangyue/PycharmProjects/bi_cron_job/backend/works/social_picture/data/train_data.p"
+    with open(train_df_fn, "rb") as p:
+        df = pickle.load(p)
+    classes_count_dict = df.groupby("label").size().reset_index(name='size').to_dict(orient='list')
+    print(classes_count_dict)
+
+    count = 0
+    for index, row in df.iterrows():
+        img_id, img_url, label = row["id"], row["url"], row["label"]
         tfrecord_name = f"trains-{img_id}.tfrecord"
         local_tf = os.path.join(LOCAL_TFRECORD_PATH, tfrecord_name)
         oss_tf = os.path.join(OssPath.TF_RECORD_PATH, tfrecord_name)
-        image_to_tfrecord(img_url, label, img_id, local_tf)
-        bucket.put_object_from_file(oss_tf, local_tf)
+        try:
+            image_to_tfrecord(img_url, label, img_id, local_tf, True)
+            if not bucket.object_exists(oss_tf):
+                bucket.put_object_from_file(oss_tf, local_tf)
+            else:
+                print("exist", oss_tf)
+            count += 1
+
+            if count % 100 == 0:
+                print(f"process {count} picture.")
+        except Exception as e:
+            print("e: ", e)
+            print(img_id, img_url)
 
 
 if __name__ == "__main__":
